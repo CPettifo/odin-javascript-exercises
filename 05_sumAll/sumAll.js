11fd2aae4e11126c85444028b40166257ad2475c
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-13 16:18:45 +0100

    finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || typeof(x) != "number" || typeof(y) != "number") {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    if (x > y) {
+        sum = x;
+        x = y;
+        y = sum;
+    }
+    sum = 0;
+    for (let i = x; i <= y; i ++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
