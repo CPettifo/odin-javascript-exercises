8c090c0c54fa9b7a98ec693930e7dc6ff2fc3ab7
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-13 16:43:13 +0100

    complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(degrees) {
+
+  degrees = Math.round((degrees * 9/5 + 32) * 10) / 10;
+  return degrees;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(degrees) {
+  degrees = Math.round(((degrees - 32) * 5/9) * 10) / 10;
+  return degrees;
 };
 
 // Do not edit below this line
