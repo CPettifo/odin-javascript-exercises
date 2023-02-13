fc06a2d3b8dc13d2e3d3171a2bcf9d3763560406
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-13 15:15:04 +0100

    complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(input, x) {
+    let string = "";
+    for (let i = 0; i < x; i++) {
+        string += input;
+    }
+    return string;
 };
 
 // Do not edit below this line
