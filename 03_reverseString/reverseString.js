48456f3be4b09a7698b3669597d05445d079c1c1
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-13 15:25:06 +0100

    complete reverseString'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length;
+    let reverse = "";
+    for (let i = length; i > -1; i --) {
+        reverse += string.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
