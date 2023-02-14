c55aaea1eae151500b767e88da358ed53e29bcf0
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-14 15:05:10 +0100

    complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+    string = string.toLowerCase();
+    string = string.replace(/[.,!]/g, "");
+    string = string.replace(/\s/g, "");
+    let j = string.length - 1;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] != string[j]) {
+            return false;
+        }
+        j-= 1;
+    }
+    return true;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
