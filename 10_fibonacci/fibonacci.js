910ccd9a46edb264a6d3bb8587406835a0915a96
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-14 15:15:39 +0100

    finish specifications fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) {
+        return "OOPS"
+    }
+    x = +x;
+    if (x === 1 || x === 2) {
+        return 1;
+    }
+    const fib = [1, 1];
+    let num = 0;
+    for (let i = 2; i < x; i++) {
+        num = fib[i-1] + fib[i-2];
+        fib.push(num);
+    }
+    return num;
 };
 
 // Do not edit below this line
