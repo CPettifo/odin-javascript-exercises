abc6f3e4731c4033070bd34d0377fca59ed91a3d
Craig Pettifor <craig_petti4@hotmail.com>
2023-02-14 15:49:24 +0100

    fiddle with code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let birth, death, oldest, oldestPerson;
+    let previous = 0;
 
+    for (let person of people) {
+        birth = person.yearOfBirth;
+        if (person.yearOfDeath == undefined) {
+            death = 2023;
+        }
+        else {
+            death = person.yearOfDeath;
+        }
+        nom = person.name;
+        oldest = death - birth;
+        if (oldest > previous) {
+            oldest = previous;
+            oldestPerson = nom;
+        }
+        previous = oldest;
+    }
+    for (let b of people) {
+        if (b.name == oldestPerson) {
+            return b;
+        }
+    }
 };
 
 // Do not edit below this line
